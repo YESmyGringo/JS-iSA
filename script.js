5700869d4e57913fb6e1818c606b43ee027d09ce
Jakub Grzybowski <kubuleskikuba@wp.pl>
2018-09-23 09:43:33 +0200

    added operation

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 var foo = 0;
-var boo = foo || "Hello"; // Hello  -- false
+var boo = foo || "Hello"; // Hello  -- false (0)
 
 var baz = 1;
-var zoo = baz || "Hello"; // 1  -- true
+var zoo = baz || "Hello"; // 1  -- true (1)
+
+
+
+var foz = false;
+var boz = !foz || 0 || 42; // true
+
+var h = (true && false) || 42; // 42
