df86a140b6a5445d4abc4be464b4ad3887c9bfea
Jakub Grzybowski <kubuleskikuba@wp.pl>
2018-09-23 10:33:44 +0200

    add swotch statement

--- script.js
+++ script.js
@@ -2,7 +2,26 @@ const vovo = "siema";
 
 if(vovo >= 1){
     console.log("It's number:", vovo);
-} 
-else if(typeof vovo == "string"){
+} else if(typeof vovo == "string"){
     console.log("It's string:", vovo);
+} else{
+    console.log("Undefinded!");
 }
+
+
+
+const value = "hello";
+
+switch(typeof value){
+    case "number":
+        console.log("It's number!:", value);
+        break;
+
+    case "string":
+        console.log("It's string!:", value);
+        break;
+
+    default:
+        console.log("Undefinded! ERROR!");
+}
+
