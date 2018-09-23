8ce2a4b6927895bbe0149f2eac6fc219697dc68c
Jakub Grzybowski <kubuleskikuba@wp.pl>
2018-09-23 10:11:09 +0200

    conditional staitments

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-var foo = 0;
-var boo = foo || "Hello"; // Hello  -- false (0)
+const vovo = "siema";
 
-var baz = 1;
-var zoo = baz || "Hello"; // 1  -- true (1)
-
-
-
-var foz = false;
-var boz = !foz || 0 || 42; // true
-
-var h = (true && false) || 42; // 42
+if(vovo >= 1){
+    console.log("It's number:", vovo);
+} 
+else if(typeof vovo == "string"){
+    console.log("It's string:", vovo);
+}
