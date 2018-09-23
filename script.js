d88b2b54ec82bb2ebee551ae0b9f7a7bee04c64e
Jakub Grzybowski <kubuleskikuba@wp.pl>
2018-09-23 09:00:42 +0200

    relational operations - Task 0

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-function sum(value1, value2) {
-    if (typeof value1 == "number" && typeof value2 == "number") {
-        return  value1 + value2;
-} else {
-    return "error";
-}
-}
+var x1 = "hello";
+var x2 = "yo, yo";
+var x3 = 200;
+var x4 = 200;
+var x5 = ['12', '14'];
+var x6 = ['60', '7']
+var x7 = [1, 6];
+var x8 = [1, 6];
+var x9 = {key: 'value1'};
+var x10 = {key: 'value1'};
 
-console.log(sum(4, "Mietek"));
+console.log("Equal strings:", x1 == x2); // false
+console.log("Equal numbers:", x3 == x4); // true
+console.log("Equal arreys length:", x5.length == x6.length); // true
+console.log("Equal tab:", x7 == x8); // false
+console.log("Equal object:", x9 == x10); //false
