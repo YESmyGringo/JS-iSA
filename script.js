ed9e20d224606a06d42e6a774b989f9b5f503dc9
Jakub Grzybowski <kubuleskikuba@wp.pl>
2018-09-23 09:25:12 +0200

    logical operators AND

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
-var x1 = "hello";
-var x2 = "yo, yo";
-var x3 = 200;
-var x4 = 200;
-var x5 = ['12', '14'];
-var x6 = ['60', '7']
-var x7 = [1, 6];
-var x8 = [1, 6];
-var x9 = {key: 'value1'};
-var x10 = {key: 'value1'};
+var foo = 0;
+var boo = foo || "Hello"; // Hello  -- false
 
-console.log("Equal strings:", x1 == x2); // false
-console.log("Equal numbers:", x3 == x4); // true
-console.log("Equal arreys length:", x5.length == x6.length); // true
-console.log("Equal tab:", x7 == x8); // false
-console.log("Equal object:", x9 == x10); //false
-
-
-// equal the values in arreys in "x7" and "x8" by JSON.stringify  
-// (can do this in arreys, keys, tab, objects ? - put they values to string)
-console.log("Equal arreys values:", JSON.stringify(x7) === JSON.stringify(x8)); // true
+var baz = 1;
+var zoo = baz || "Hello"; // 1  -- true
