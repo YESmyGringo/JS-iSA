d091ee573be6d2f1539b9226cef9cb1628d0eb62
Jakub Grzybowski <kubuleskikuba@wp.pl>
2018-09-29 09:26:53 +0200

    changes

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const a = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let i = 0;
+//let i = 0;
 
-while(i < a.length) {
-    console.log(a[i])
-    i++;
+//while(i < a.length) {
+//    console.log(a[i])
+//    i++;
+//}
+
+for(let value of a){
+    console.log('Value:', a);
+}
+
+for(let index in a) {
+    console.log('Index:', a);
 }
