5d33b54a86e062f469fb6df257094820ffdbd14e
Jakub Grzybowski <kubuleskikuba@wp.pl>
2018-09-29 09:57:47 +0200

    break in 1 and show two numers (index [0] and [1])

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
-//let i = 0;
+const values = [];
 
-//while (i < 5) {
-//    i++;
-//    if (i === 3) {  // przeskakuje liczbe 3 i idzie dalej
-//        continue;
-//    }
-//    console.log(i);
-//}
-
-
-var numbers = [1,2,3,4,5];
-
-for (var i = 0; i < numbers.length; i++) {
-    if (numbers[i] === 3) {  // przy 3 zatrzymuje funkcje
-        break;
-    }
-    console.log(numbers[i]);
+for(let i = 0; i < 10; i++) {
+    const randomValues = Math.floor(Math.random() * 1000) + 1
+    values.push(randomValues);
+if(i === 1){ // zatrzymuje sie na 1 i wyswietla dwie wartosci
+    break;
 }
+}
+    console.log(values); 
