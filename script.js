98fdcce685503b87339c1423b3817e0997b3d34e
Jakub Grzybowski <kubuleskikuba@wp.pl>
2018-09-29 08:59:55 +0200

    added comment

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const values = [];
 
+// pętla wyświetla losową liczbe od 1 do 1000. pokazuje 10 liczb "i = 0; i < 10; i++"
+
 for(let i = 0; i < 10; i++) {
     const randomValues = Math.floor(Math.random() * 1000) + 1
     values.push(randomValues);
