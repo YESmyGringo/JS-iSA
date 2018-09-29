4b8ae6c20a8b83025d97a6fb63beef6d07fd543e
Jakub Grzybowski <kubuleskikuba@wp.pl>
2018-09-29 08:29:36 +0200

    speed test

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 var numbers = [1, 2, 3, 4, 5];
 
-for (var i = 0; i < 4; i++)
+console.time('in loop')
+for (var i = 0; i < 4; i++){
 console.log(numbers[i])
+}
+console.timeEnd('in loop')
 
-for (var i = 0; i < numbers.length; i++) {
+console.time('before loop')
+var lenght = numbers.lenght
+for (var i = 0; i < length; i++) {
     console.log(numbers[i])
 }
+console.timeEnd('before loop')
