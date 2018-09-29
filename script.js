6397f115ec2ceefb3fbe0b08f27b19f67960f236
Jakub Grzybowski <kubuleskikuba@wp.pl>
2018-09-29 11:19:52 +0200

    tablica

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var c = [2,1,2,3,7];
+const a = [1,2,3, ["a","b","c"]];  // --- drugi array na pozycji [3] w pierwszym
+// const b = a; --- pokazuja sie dwie takie same tablice w console.log
+const b = a.slice(0, a.length);
 
+b[3].push("d"); // --- dodajemy do [3] pozcycji na której jest array "d" =  [1,2,3, ["a", "b", "c", "d"]]
 
-console.log(c);
+console.log(a,b);
 
-c.unshift(0);
-c.push(6);
-
-
-console.log(c);
+var sliced = a.slice(1, 3); // --- wkazujemy początek i koniec wyświetlania z indexu. do 3 ponieważ +1.
+console.log(sliced);
